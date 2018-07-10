e3a688f671a9c53dd9899d9862c63d0f66800978
el-guero <mario.fontes0825@gmail.com>
2018-07-09 19:09:18 -0600

    FinallyFixedTable_of_contents

--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -1,12 +1,10 @@
 title = ['Hot, Flat and Crowded Table of Contents']
-chapters = ['3: How We Move Forward', '2: How We Got There' , '1: Where We Are']
-pages = ['page 3', 'page 53' , 'page 203']
-while pages != []
-  puts title[0].center(80)
- puts chapters[2].ljust(0) + pages[2].rjust(50)
- pages.pop
- puts chapters[1].ljust(0) + pages[1].rjust(46)
- pages.pop
- puts chapters[0].ljust(0) + pages[0].rjust(43)
- pages.pop
+chapters = ['1: Where We Are       ', '2: How We Got There   ', '3: How We Move Forward']
+pages = ['page 3', 'page 53' , 'page 206']
+number = 0
+
+puts title[0].center(80)
+while number != 3
+ puts chapters[number].ljust(0) + pages[number].rjust(50)
+ number += 1
 end
